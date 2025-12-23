0eb680db272900cdfe2f404ab823e58d0533d962
Evan Bourgoine <evan.bourgoine@gmail.com>
2025-12-23 16:04:25 -0500

    Initial Commit

---
+++ Wave/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Wave
+//
+//  Created by Evan M Bourgoine on 12/23/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Wave/WaveApp.swift
@@ -0,0 +1,17 @@
+//
+//  WaveApp.swift
+//  Wave
+//
+//  Created by Evan M Bourgoine on 12/23/25.
+//
+
+import SwiftUI
+
+@main
+struct WaveApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
